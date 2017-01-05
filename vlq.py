9ca955e3aa18b6095fd23d7099b5d15a01806e83
Klaas van Schelven <klaas@vanschelven.com>
2017-01-05 20:17:31 +0100

    Updated the VLQ definition to fit the one from Wikipedia (Midi)
    Better not to have to explain why I'm deviating from the standard, even if the
    difference is not meaningful

--- vlq.py
+++ vlq.py
@@ -10,50 +10,50 @@ If the MSB is 0, then this is the last VLQ octet of the integer. If A is 1, then
 arranged most significant first in a stream.
 
 
-## Klaas' spin on this:
-
-I've taken the opposite approach from the Wiki definition, putting the least significant byte first.
-
-Implementation seems to be much more straightforward if the least significant bits are first.  Perhaps this is simply a
-rehashing of the old war of little and big endianness.  In any case... this is perhaps simply my lazyness, I might
-reconsider.
-
->>> interesting = [0, 1, 42, 127, 128, 128 * 128 - 1, 128 * 128, 1234567890]
+>>> interesting = [0, 1, 42, 0x7f, 0x80, 0x2000, 0x3fff, 0x4000, 1234567890]
 >>> for i in interesting:
 ...     print("%12d: %s" % (i, to_vlq(i)))
 ...     assert from_vlq(iter(to_vlq(i))) == i
 ...
-           0: b'\x00'
-           1: b'\x01'
-          42: b'*'
-         127: b'\x7f'
-         128: b'\x80\x01'
-       16383: b'\xff\x7f'
-       16384: b'\x80\x80\x01'
-  1234567890: b'\xd2\x85\xd8\xcc\x04'
+               0: b'\x00'
+               1: b'\x01'
+              42: b'*'
+             127: b'\x7f'
+             128: b'\x81\x00'
+            8192: b'\xc0\x00'
+           16383: b'\xff\x7f'
+           16384: b'\x81\x80\x00'
+      1234567890: b'\x84\xcc\xd8\x85R'
+
 """
 
 
 def to_vlq(i):
+    needed_bytes = 1
     result = b''
 
-    while True:
-        result += bytes([(i % 128) + (128 if i >= 128 else 0)])
-        i = i // 128
-        if i == 0:
-            return result
+    while pow(128, needed_bytes) <= i:
+        needed_bytes += 1
+
+    mod = None
+    for j in reversed(range(needed_bytes)):
+        div = pow(128, j)
+        result += bytes([(i % mod if mod else i) // div + (128 if j > 0 else 0)])
+        mod = div
+
+    return result
 
 
 def from_vlq(bytes_stream):
     # bytes_stream is a bytes_stream of bytes, on which next(...) can be called which yields a byte
     result = 0
-    base = 1
 
     while True:
         b = next(bytes_stream)
 
-        result += (b % 128) * base
-        base *= 128
+        result += (b % 128)
 
         if b < 128:
             return result
+
+        result *= 128
