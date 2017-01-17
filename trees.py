4bcdc135afd0c1f12771e5c0c76a45cc11f0dbc9
Klaas van Schelven <klaas@vanschelven.com>
2017-01-17 17:33:30 +0100

    Factor out pretty printing methods from the TreeNode

--- trees.py
+++ trees.py
@@ -21,50 +21,10 @@ class TreeNode(object):
         self.metadata = metadata
 
     def __repr__(self):
-        return self.pp_flat()
-
-    # TODO I'd rather factor out pp*
-
-    def pp_flat(self):
-        return "(" + " ".join(c.pp_flat() for c in self.children) + ")"
-
-    def pp_2(self, indentation):
-        # "Lisp Style indentation, i.e. xxx yyy
-        #                                   zzz
-        if len(self.children) <= 2:
-            return "(" + " ".join(c.pp_flat() for c in self.children) + ")"
-
-        my_arg_0 = "(" + self.children[0].pp_flat()  # the first element is always shown flat;
-        next_indentation = indentation + len(my_arg_0) + len(" ")
-
-        return (my_arg_0 + " " + self.children[1].pp_2(next_indentation) + "\n" +
-                "\n".join((" " * next_indentation) + c.pp_2(next_indentation) for c in self.children[2:]) + ")")
-
-    def pp_todo(self, indentation):
-        if len(self.children) < 1:
-            return "(...)"
-
-        # a somewhat unexpect scenario, because the first arg is supposed to be text in this setup
-        my_arg_0 = "" + self.children[0].pp_flat()
-        next_indentation = indentation + 4
-
-        return (my_arg_0 + ("\n\n" if len(self.children) > 1 else "") +
-                "\n\n".join((" " * next_indentation) + c.pp_todo(next_indentation) for c in self.children[1:]))
-
-    def pp_todo_numbered(self, indentation):
-        if len(self.children) < 1:
-            return "(...)"
-
-        # a somewhat unexpect scenario, because the first arg is supposed to be text in this setup
-        my_arg_0 = "[0] " + self.children[0].pp_flat()
-        next_indentation = indentation + 4
-
-        return (my_arg_0 + ("\n\n" if len(self.children) > 1 else "") +
-                "\n\n".join(
-                    (" " * next_indentation) + "[%s] " % (i + 1) + c.pp_todo(next_indentation)
-                    for (i, c) in enumerate(self.children[1:])))
+        return pp_flat(self)
 
     def as_bytes(self):
+        # as of yet unused
         return bytes([TREE_NODE]) + to_vlq(len(self.children)) + b''.join([c.as_bytes() for c in self.children])
 
 
@@ -76,21 +36,66 @@ class TreeText(object):
         self.metadata = metadata
 
     def __repr__(self):
-        return self.unicode_
-
-    def pp_flat(self):
-        return self.unicode_
-
-    def pp_2(self, indentation):
-        return self.unicode_
-
-    def pp_todo(self, indentation):
-        return self.unicode_
-
-    def pp_todo_numbered(self, indentation):
-        return self.unicode_
+        return pp_flat(self)
 
     def as_bytes(self):
         # as of yet unused
         utf8 = self.unicode_.encode('utf-8')
         return bytes([TREE_TEXT]) + to_vlq(len(utf8)) + utf8
+
+
+# Tools for Pretty Printing
+
+def pp_flat(node):
+    if isinstance(node, TreeText):
+        return node.unicode_
+    return "(" + " ".join(pp_flat(c) for c in node.children) + ")"
+
+
+def pp_2(node, indentation):
+    """"Lisp Style indentation, i.e. xxx yyy
+                                     zzz
+    """
+    if isinstance(node, TreeText):
+        return node.unicode_
+
+    if len(node.children) <= 2:
+        return "(" + " ".join(pp_flat(c) for c in node.children) + ")"
+
+    my_arg_0 = "(" + pp_flat(node.children[0])  # the first element is always shown flat;
+    next_indentation = indentation + len(my_arg_0) + len(" ")
+
+    return (my_arg_0 + " " + pp_2(node.children[1], next_indentation) + "\n" +
+            "\n".join((" " * next_indentation) + pp_2(c, next_indentation) for c in node.children[2:]) + ")")
+
+
+def pp_todo(node, indentation):
+    if isinstance(node, TreeText):
+        return node.unicode_
+
+    if len(node.children) < 1:
+        return "(...)"
+
+    # a somewhat unexpect scenario, because the first arg is supposed to be text in this setup
+    my_arg_0 = "" + pp_flat(node.children[0])
+    next_indentation = indentation + 4
+
+    return (my_arg_0 + ("\n\n" if len(node.children) > 1 else "") +
+            "\n\n".join((" " * next_indentation) + pp_todo(c, next_indentation) for c in node.children[1:]))
+
+
+def pp_todo_numbered(node, indentation):
+    if isinstance(node, TreeText):
+        return node.unicode_
+
+    if len(node.children) < 1:
+        return "(...)"
+
+    # a somewhat unexpect scenario, because the first arg is supposed to be text in this setup
+    my_arg_0 = "[0] " + pp_flat(node.children[0])
+    next_indentation = indentation + 4
+
+    return (my_arg_0 + ("\n\n" if len(node.children) > 1 else "") +
+            "\n\n".join(
+                (" " * next_indentation) + "[%s] " % (i + 1) + pp_todo(c, next_indentation)
+                for (i, c) in enumerate(node.children[1:])))
