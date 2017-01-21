2997b17388c10137c901414eab4ca51baf150886
Klaas van Schelven <klaas@vanschelven.com>
2017-01-21 14:18:52 +0100

    Factor out 4 functions for Node-manipulation to a general file
    rather than to have them specific to the idea of 'editor-specific stuff'

--- dsn/editor/construct.py
+++ dsn/editor/construct.py
@@ -1,15 +1,16 @@
 from s_address import node_for_s_address, s_dfs
-from hashstore import Hash
-from posacts import Possibility, Actuality
-from legato import NoutBegin, NoutBlock
-from clef import Replace
 
-from clef import (
-    BecomeNode,
-    Delete,
-    Insert,
-    TextBecome,
+from legato import NoutBlock
+from manipulations import (
+    bubble_history_up,
+    calc_possibility,
+    insert_text_at,
+    insert_node_at,
+    replace_text_at,
 )
+
+from clef import Delete
+
 from trees import TreeNode
 
 from dsn.editor.clef import (
@@ -23,88 +24,6 @@ from dsn.editor.clef import (
     TextInsert,
     TextReplace,
 )
-
-
-def calc_possibility(nout):
-    # Note: the're some duplication here of logic that's also elsewhere, e.g. the calculation of the hash was
-    # copy/pasted from the HashStore implementation; but we need it here again.
-
-    bytes_ = nout.as_bytes()
-    hash_ = Hash.for_bytes(bytes_)
-    return Possibility(nout), hash_
-
-
-def calc_actuality(nout_hash):
-    return Actuality(nout_hash)
-
-
-def bubble_history_up(hash_to_bubble, tree, s_address):
-    """Recursively replace history to reflect a change (hash_to_bubble) at a lower level (s_address)"""
-
-    posacts = []
-    for i in reversed(range(len(s_address))):
-        # We slide a window of size 2 over the s_address from right to left, like so:
-        # [..., ..., ..., ..., ..., ..., ...]  <- s_address
-        #                              ^  ^
-        #                           [:i]  i
-        # For each such i, the sliced array s_address[:i] gives you the s_address of a node in which a replacement
-        # takes place, and s_address[i] gives you the index to replace at.
-        #
-        # Regarding the range (0, len(s_address)) the following:
-        # * len(s_address) means the s_address itself is the first thing to be replaced.
-        # * 0 means: the last replacement is _inside_ the root node (s_address=[]), at index s_address[0]
-        replace_in = node_for_s_address(tree, s_address[:i])
-
-        p, hash_to_bubble = calc_possibility(
-            NoutBlock(Replace(s_address[i], hash_to_bubble), replace_in.metadata.nout_hash))
-
-        posacts.append(p)
-
-    # The root node (s_address=[]) itself cannot be replaced, its replacement is represented as "Actuality updated"
-    posacts.append(calc_actuality(hash_to_bubble))
-    return posacts
-
-
-# TODO: insert_xxx_at: I see a pattern here!
-def insert_text_at(tree, parent_s_address, index, text):
-    parent_node = node_for_s_address(tree, parent_s_address)
-
-    pa0, begin = calc_possibility(NoutBegin())
-    pa1, to_be_inserted = calc_possibility(NoutBlock(TextBecome(text), begin))
-
-    pa2, insertion = calc_possibility(
-        NoutBlock(Insert(index, to_be_inserted), parent_node.metadata.nout_hash))
-
-    posacts = bubble_history_up(insertion, tree, parent_s_address)
-    return [pa0, pa1, pa2] + posacts
-
-
-def insert_node_at(tree, parent_s_address, index):
-    parent_node = node_for_s_address(tree, parent_s_address)
-
-    pa0, begin = calc_possibility(NoutBegin())
-    pa1, to_be_inserted = calc_possibility(NoutBlock(BecomeNode(), begin))
-
-    pa2, insertion = calc_possibility(
-        NoutBlock(Insert(index, to_be_inserted), parent_node.metadata.nout_hash))
-
-    posacts = bubble_history_up(insertion, tree, parent_s_address)
-    return [pa0, pa1, pa2] + posacts
-
-
-def replace_text_at(tree, s_address, text):
-    parent_node = node_for_s_address(tree, s_address[:-1])
-
-    pa0, begin = calc_possibility(NoutBegin())
-    pa1, to_be_inserted = calc_possibility(NoutBlock(TextBecome(text), begin))
-
-    index = s_address[-1]
-
-    pa2, insertion = calc_possibility(
-        NoutBlock(Replace(index, to_be_inserted), parent_node.metadata.nout_hash))
-
-    posacts = bubble_history_up(insertion, tree, s_address[:-1])
-    return [pa0, pa1, pa2] + posacts
 
 
 def edit_note_play(structure, edit_note):
---
+++ manipulations.py
@@ -0,0 +1,93 @@
+from clef import (
+    BecomeNode,
+    Insert,
+    Replace,
+    TextBecome,
+)
+
+from hashstore import Hash
+from legato import NoutBegin, NoutBlock
+from posacts import Possibility, Actuality
+from s_address import node_for_s_address
+
+
+def calc_possibility(nout):
+    # Note: the're some duplication here of logic that's also elsewhere, e.g. the calculation of the hash was
+    # copy/pasted from the HashStore implementation; but we need it here again.
+
+    bytes_ = nout.as_bytes()
+    hash_ = Hash.for_bytes(bytes_)
+    return Possibility(nout), hash_
+
+
+def calc_actuality(nout_hash):
+    return Actuality(nout_hash)
+
+
+def bubble_history_up(hash_to_bubble, tree, s_address):
+    """Recursively replace history to reflect a change (hash_to_bubble) at a lower level (s_address)"""
+
+    posacts = []
+    for i in reversed(range(len(s_address))):
+        # We slide a window of size 2 over the s_address from right to left, like so:
+        # [..., ..., ..., ..., ..., ..., ...]  <- s_address
+        #                              ^  ^
+        #                           [:i]  i
+        # For each such i, the sliced array s_address[:i] gives you the s_address of a node in which a replacement
+        # takes place, and s_address[i] gives you the index to replace at.
+        #
+        # Regarding the range (0, len(s_address)) the following:
+        # * len(s_address) means the s_address itself is the first thing to be replaced.
+        # * 0 means: the last replacement is _inside_ the root node (s_address=[]), at index s_address[0]
+        replace_in = node_for_s_address(tree, s_address[:i])
+
+        p, hash_to_bubble = calc_possibility(
+            NoutBlock(Replace(s_address[i], hash_to_bubble), replace_in.metadata.nout_hash))
+
+        posacts.append(p)
+
+    # The root node (s_address=[]) itself cannot be replaced, its replacement is represented as "Actuality updated"
+    posacts.append(calc_actuality(hash_to_bubble))
+    return posacts
+
+
+# TODO: insert_xxx_at: I see a pattern here!
+def insert_text_at(tree, parent_s_address, index, text):
+    parent_node = node_for_s_address(tree, parent_s_address)
+
+    pa0, begin = calc_possibility(NoutBegin())
+    pa1, to_be_inserted = calc_possibility(NoutBlock(TextBecome(text), begin))
+
+    pa2, insertion = calc_possibility(
+        NoutBlock(Insert(index, to_be_inserted), parent_node.metadata.nout_hash))
+
+    posacts = bubble_history_up(insertion, tree, parent_s_address)
+    return [pa0, pa1, pa2] + posacts
+
+
+def insert_node_at(tree, parent_s_address, index):
+    parent_node = node_for_s_address(tree, parent_s_address)
+
+    pa0, begin = calc_possibility(NoutBegin())
+    pa1, to_be_inserted = calc_possibility(NoutBlock(BecomeNode(), begin))
+
+    pa2, insertion = calc_possibility(
+        NoutBlock(Insert(index, to_be_inserted), parent_node.metadata.nout_hash))
+
+    posacts = bubble_history_up(insertion, tree, parent_s_address)
+    return [pa0, pa1, pa2] + posacts
+
+
+def replace_text_at(tree, s_address, text):
+    parent_node = node_for_s_address(tree, s_address[:-1])
+
+    pa0, begin = calc_possibility(NoutBegin())
+    pa1, to_be_inserted = calc_possibility(NoutBlock(TextBecome(text), begin))
+
+    index = s_address[-1]
+
+    pa2, insertion = calc_possibility(
+        NoutBlock(Replace(index, to_be_inserted), parent_node.metadata.nout_hash))
+
+    posacts = bubble_history_up(insertion, tree, s_address[:-1])
+    return [pa0, pa1, pa2] + posacts
